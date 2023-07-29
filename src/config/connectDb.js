1079991ebdb5ad059b28601885e1df4c7e22015a
hien_dang <thehien9299@gmail.com>
2023-07-29 15:04:18 +0700

    deloy api

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,5 +1,4 @@
-import { Sequelize } from "sequelize"
-
+const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
